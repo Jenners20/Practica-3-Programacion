955ba65f35722b5d53ae96934419afdbc1d2571a
Jenners20 <61439419+Jenners20@users.noreply.github.com>
2020-05-26 20:39:49 -0400

    Add files via upload

---
+++ Ejercicio2/Ejercicio2/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Crear un programa que pida al usuario ds numeros enteros
+            //y diga cual es el mayor de ellos
+
+            int num1 = 0;
+            int num2 = 0;
+
+            Console.Write("Ingrese un numero: ");
+            num1 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            Console.Write("Ingrese un numero: ");
+            num2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            if (num1>num2)
+            {
+                Console.WriteLine("El numero mayor es: " + num1);
+            }
+            if (num1<num2)
+            {
+                Console.WriteLine("El numero mayor es: " + num2);
+            }
+            if (num1==num2)
+            {
+                Console.WriteLine("Los numeros {0} y {1} son iguales ",num1,num2);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
