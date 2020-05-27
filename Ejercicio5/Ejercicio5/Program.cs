494a97ce89cce4dce1f9d2dee5e3521ded19809a
Jenners20 <61439419+Jenners20@users.noreply.github.com>
2020-05-26 20:47:02 -0400

    Add files via upload

---
+++ Ejercicio5/Ejercicio5/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Crear un programa que multiplique dos números
+             * enteros de la siguiente forma: pedirá al usuario un
+             * primer número entero. Si el número que se que teclee
+             * es 0, escribirá en pantalla "El producto de 0 por 
+             * cualquier número es 0". Si se ha tecleado un número
+             * distinto de cero, se pedirá al usuario un segundo 
+             * número y se mostrará el producto de ambos.*/
+
+            int[] numeros=new int[2];
+            int resultado = 0;
+
+            Console.Write("Ingrese un numero entero: ");
+            numeros[0] = int.Parse(Console.ReadLine());
+
+            Console.WriteLine();
+
+            if (numeros[0]!=0)
+            {
+                Console.Write("Ingrese un numero entero: ");
+                numeros[1] = int.Parse(Console.ReadLine());
+
+                Console.WriteLine();
+
+                resultado = numeros[0] * numeros[1];
+
+                Console.WriteLine("El producto de {0} y {1} es: {2}", numeros[0], numeros[1], resultado);
+            }
+            else
+            {
+                Console.WriteLine("El producto de 0 por cualquier número es 0");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
