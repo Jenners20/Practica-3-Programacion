c22c2c536fb5e3aaccb98ae6c0a91bbbc8fda857
Jenners20 <61439419+Jenners20@users.noreply.github.com>
2020-05-26 20:49:43 -0400

    Add files via upload

---
+++ Ejercicio7/Ejercicio7/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ejercicio7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*Crear un programa que pida al usuario
+             * un número entero y diga si es múltiplo de 4 o de 5. */
+            int numero = 0;
+            double multiplo;
+
+            Console.Write("Ingrese un numero entero: ");
+            numero = int.Parse(Console.ReadLine());
+
+            multiplo = numero % 4;
+
+            if (multiplo==0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("El numero {0} es multiplo de 4 ",numero);
+            }
+            else
+            {
+                Console.WriteLine("El numero {0} no es multiplo de 4 ", numero);
+            }
+
+            multiplo = numero % 5;
+
+            if (multiplo == 0)
+            {
+                Console.WriteLine();
+                Console.WriteLine("El numero {0} es multiplo de 5 ",numero);
+            }
+            else
+            {
+                Console.WriteLine("El numero {0} no es multiplo de 5 ",numero);
+            }
+            Console.ReadKey();
+        }
+    }
+}
